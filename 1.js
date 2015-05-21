8f2816508400132a85f6718236abe942be0d08ce
gunrugger <gun.rugger@gmail.com>
2015-05-20 22:10:10 -0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,19 +1,19 @@
 // problem: why does Ralph only bark once?
 // fix it to make Ralph bark twice!
 
-var Dog = function( name ) {
-  this.name = name
+var dog = function( name ) {
+  this.name = name;
 }
 
-Dog.prototype.bark = function() {
-  console.log( this.name )
+dog.prototype.bark = function() {
+  console.log( this.name );
 }
 
 // make a new instance of a Dog
-var ralph = new Dog('Ralph')
+var ralph = new dog('Ralph');
 
 // make Ralph bark once immediately
-ralph.bark()
+ralph.bark();
 
 // in 1 second we want Ralph to bark again
-setTimeout( ralph.bark , 1000 )
+setTimeout( ralph.bark() , 1000 );
